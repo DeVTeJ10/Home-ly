90a472af5ee12598051d7559b1a8eb617beb9f61
Oru <tejirioru@gmail.com>
2024-07-23 18:26:10 +0100

    minor footer edits

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,30 +7,62 @@ import './footer.css'
 const index = () => {
   return (
     <div>
+
+        <div className='finalfooter'>
         <div className='sentient'>
-            <div className='logo2s'>
+        <div className='logo2s'>
                 <img src="./src/images/Logo.png" width={113.33} height={34} />
-            </div>
-            <div className='features'>
-                <p>Home</p>
-                <p>About Us</p>
-                <p>Properties</p>
-                <p>Services</p>
-                <p>Contact Us</p>
-            </div>
+        </div>
         </div>
 
-        <div className='sentients'>
-            <div className=''>
-                <input placeholder='Enter Your Email' className='emails'></input>
-            </div>
-            <div className='feature2'>
+        <div className='mailedit'>
+        </div>
+
+
+        <div className='features1'>
+        <div className='features'>
                 <p>Home</p>
-                <p>About Us</p>
+                <p>Hero Section</p>
+                <p>Features</p>
                 <p>Properties</p>
+                <p>Testimonials</p>
+                <p>FAQs</p>
+        </div>
+        </div>
+        <div className='features1'>
+        <div className='features'>
+                <p>About Us</p>
+                <p>Our Story</p>
+                <p>Our Works</p>
+                <p>How It Works</p>
+                <p>Our Team</p>
+                <p>Our Clients</p>
+        </div>
+        </div>
+        <div className='features1'>
+        <div className='features'>
+                <p>Properties</p>
+                <p>Portfolios</p>
+                <p>categories</p>
+        </div>
+        </div>
+        <div className='features1'>
+        <div className='features'>
                 <p>Services</p>
+                <p>Valuation Mastery</p>
+                <p>Strategic Marketing</p>
+                <p>Negotiation Wizardry</p>
+                <p>Closing Success</p>
+                <p>Property Management</p>
+        </div>
+        </div>
+        <div className='features1'>
+        <div className='features'>
                 <p>Contact Us</p>
-            </div>
+                <p>Contact Form</p>
+                <p>Our Offices</p>
+        </div>
+        </div>
         </div>
     </div>
   )
