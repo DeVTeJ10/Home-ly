05721fe8edf21a1a07ac29257ff084820f0dc5ca
Oru <tejirioru@gmail.com>
2024-07-23 17:26:47 +0100

    footer design

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -7,22 +7,31 @@ import './footer.css'
 const index = () => {
   return (
     <div>
-        <div className='mailogo'>
-            <div>
-                
+        <div className='sentient'>
+            <div className='logo2s'>
+                <img src="./src/images/Logo.png" width={113.33} height={34} />
             </div>
-        <div className='logo2'>
-        <img src="./src/images/Logo.png" width={113.33} height={34} className='logo2s'/>
-
+            <div className='features'>
                 <p>Home</p>
                 <p>About Us</p>
                 <p>Properties</p>
                 <p>Services</p>
                 <p>Contact Us</p>
+            </div>
         </div>
-        </div>
-            <input placeholder='Enter Your Email' className='emails'></input>
 
+        <div className='sentients'>
+            <div className=''>
+                <input placeholder='Enter Your Email' className='emails'></input>
+            </div>
+            <div className='feature2'>
+                <p>Home</p>
+                <p>About Us</p>
+                <p>Properties</p>
+                <p>Services</p>
+                <p>Contact Us</p>
+            </div>
+        </div>
     </div>
   )
 }
