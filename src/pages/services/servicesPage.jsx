c1c226e16b79245744244ea2c73aca267bdb668e
Oru <tejirioru@gmail.com>
2024-08-14 16:02:16 +0100

    still refactoring

--- src/pages/services/servicesPage.jsx
+++ src/pages/services/servicesPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../../components/Header/index.jsx";
+import Header from "../../components/Header";
 import DreamHome from "../../components/DreamHome";
 import Footer from "../../components/Footer"
 import "./servicesPage.css";
