c5c93323c8125c8b1db9e4220b32fd6f6b4245ee
Oru <tejirioru@gmail.com>
2024-08-16 16:37:51 +0100

    update

--- src/pages/services/servicesPage.jsx
+++ src/pages/services/servicesPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "../../components/header";
-import DreamHome from "../../components/dreamHome";
-import Footer from "../../components/Footer"
+import Footer from "../../components/footer"
 import "./servicesPage.css";
 
 // Import all images
