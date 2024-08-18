18e70079a008d6e1540cf01b7435ad5ad44c9166
Oru <tejirioru@gmail.com>
2024-08-18 02:36:09 +0100

    refactoring codes, moving files to necessary locations

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
-    port: 5173,
-    host: true
+    port: 3000,
+    host: true,
+    open: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
   }
 })
